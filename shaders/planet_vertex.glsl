e918044b4df7e14819bdcabe55fbd591e5ae121e
Klas Eskilson <klas.eskilson@gmail.com>
2016-01-04 16:04:24 +0100

    Add simple vertex noise

--- shaders/planet_vertex.glsl
+++ shaders/planet_vertex.glsl
@@ -1,6 +1,22 @@
+#ifdef GL_ES
+  precision highp float;
+#endif
+
+/*
+// default vertex attributes
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec2 uv;
+attribute vec2 uv2;
+*/
+
 varying vec2 vUv;
 
 void main() {
+  float intensity = 20.0;
+  vec3 offset = normal * intensity * snoise(0.3 * position);
+  vec3 pos = position + offset;
+
   vUv = uv;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
